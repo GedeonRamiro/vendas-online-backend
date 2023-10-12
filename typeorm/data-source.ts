d3c38e310a9542a0e32457ca2a8c5d3f3386f237
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2023-10-12 13:35:19 -0300

    refactor: mysql -> sqlite3

--- typeorm/data-source.ts
+++ typeorm/data-source.ts
@@ -6,11 +6,7 @@ dotenv.config({
 });
 
 const dataSouce = new DataSource({
-  type: 'mysql',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
+  type: 'sqlite',
   database: process.env.DB_DATABASE,
   migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
 });
