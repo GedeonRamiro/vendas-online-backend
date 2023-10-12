70e5fa72997d78d9cd2d95b7046b392871f2da6c
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2023-10-12 13:34:14 -0300

    fix: LoginDto

--- src/auth/dtos/LoginDto.ts
+++ src/auth/dtos/LoginDto.ts
@@ -1,4 +1,10 @@
+import { IsString, MaxLength, maxLength, MinLength } from 'class-validator';
+
 export class LoginDto {
+  @IsString()
   email: string;
+
+  @IsString()
+  @MinLength(6)
   password: string;
 }
