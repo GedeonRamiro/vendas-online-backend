8db0d67c6cde25c4af6223f3ae8774d16c7c5844
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2023-10-12 13:33:55 -0300

    refactor: mysql -> sqlite3

--- src/app.module.ts
+++ src/app.module.ts
@@ -24,11 +24,7 @@ import { OrderProductModule } from './order-product/order-product.module';
       envFilePath: '.env',
     }),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
+      type: 'sqlite',
       database: process.env.DB_DATABASE,
       entities: [`${__dirname}/**/*.entity{.js,.ts}`],
     }),
